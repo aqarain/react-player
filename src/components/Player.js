b98bb4449b82bfe8a8fa8b7e80b563791048e8d0
Atif Qayyum <aqarain92@gmail.com>
2021-02-01 03:27:42 +0500

    customizing the slider

--- src/components/Player.js
+++ src/components/Player.js
@@ -63,17 +63,29 @@ const Player = ({
     playAudio(isPlaying, audioRef);
   };
 
+  const trackAnim = {
+    transform: `translate(${songInfo.animationPercentage}%)`
+  };
+
   return (
     <div className="player">
       <div className="time-control">
         <p> {getTime(songInfo.currentTime)} </p>
-        <input
-          type="range"
-          min={0}
-          max={songInfo.duration || 0}
-          value={songInfo.currentTime}
-          onChange={dragHandler}
-        />
+        <div
+          className="track"
+          style={{
+            background: `linear-gradient(to right, ${currentSong.color[0]}, ${currentSong.color[1]} )`
+          }}
+        >
+          <input
+            type="range"
+            min={0}
+            max={songInfo.duration || 0}
+            value={songInfo.currentTime}
+            onChange={dragHandler}
+          />
+          <div style={trackAnim} className="animate-track"></div>
+        </div>
         <p> {songInfo.duration ? getTime(songInfo.duration) : "0:00"} </p>
       </div>
       <div className="play-control">
